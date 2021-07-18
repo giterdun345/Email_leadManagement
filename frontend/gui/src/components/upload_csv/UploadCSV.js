f5760ecfbc0280ffb54499188afd01a3f1d334d1
John Ketterer <johnmketterer@gmail.com>
2021-07-18 12:39:31 -0500

    upload csv functionality

--- frontend/gui/src/components/upload_csv/UploadCSV.js
+++ frontend/gui/src/components/upload_csv/UploadCSV.js
@@ -8,26 +8,25 @@ const UploadCSV = () => {
   const [convertedData, setConvertedData] = useState([])
   // Add the file to a formData object, and set the Content-Type header to multipart/form-data.
 
+  const updateData= (results)=>{
+      setConvertedData(results.data)
+      
+  }
+
   const convertFile= ({file, onSuccess})=>{
   // custom override of Upload to convert from csv to json before bulk post 
+  // state could not be updated in parse function  
     papa.parse(file,  {
       header: true,
-      // xlxs = "ISO-8859-1"
-      // encoding:'UTF-8', 
       skipEmptyLines: true,
-      complete: function(results) {
-        setConvertedData(results.data)
-        console.log('In convert', convertedData)
-        onSuccess("ok");;
-      }
+      error: (err) => console.log(err.message),
+      complete: updateData && onSuccess('ok')
     })
   }
 
   const submitConvertedData= ()=>{
-    console.log('In Submit', convertedData)
     axios.post('http://localhost:8000/api/target/', convertedData)
     .then(res => {
-      // console.log(res)
       alert(`${res.status}, ${res.statusText}: ${res.data.name}`)
       setConvertedData([])
     })
@@ -47,13 +46,11 @@ const formItemLayout = {
 };
 
 const normFile = (e) => {
-  // console.log('Upload event:', e);
   if (Array.isArray(e)) {
     return e;
   }else{
     return e && e.fileList;
   }
-  
 };
 
   const onFinish = (values) => {
@@ -64,26 +61,28 @@ const normFile = (e) => {
     <Form name="csv_upload" {...formItemLayout} onFinish={onFinish} >
       <Form.Item>
         <Form.Item name="dragger" valuePropName="fileList" getValueFromEvent={normFile} noStyle>
+          
           <Upload.Dragger name="files" customRequest={convertFile}>
             <p className="ant-upload-drag-icon">
               <InboxOutlined />
             </p>
             <p className="ant-upload-text">Click or drag file to this area to upload</p>
-            <p className="ant-upload-hint">Support for a single or bulk upload.</p>
+            <p className="ant-upload-hint">Support for a single file or bulk upload.</p>
           </Upload.Dragger>
+
         </Form.Item>
       </Form.Item>
 {/* BUTTON FOR SUBMISSION TO BACKEND TARGET MODEL  */}
-      {/* <Form.Item wrapperCol={{ span: 12, offset: 6 }}> */}
+      <Form.Item wrapperCol={{ span: 12 }}>
         <Button type="primary" htmlType="submit"  onClick={submitConvertedData}>
           Submit
         </Button>
-      {/* </Form.Item> */}
-      {/* <Form.Item wrapperCol={{ span: 12, offset: 6 }}> */}
-        <Button type="primary" htmlType="button">
+      </Form.Item>
+      <Form.Item wrapperCol={{ span: 12 }}>
+        <Button type="primary" htmlType="button" onClick={()=> alert('Feature Soon Come')}>
           Pull From Google Sheets
         </Button>
-      {/* </Form.Item> */}
+      </Form.Item>
     </Form>
    );
 }
